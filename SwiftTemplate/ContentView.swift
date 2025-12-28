fdf1770b81ce5a7407ebc6c5fbf6aa0bc7ac8f9f
Adrian <adrian.neshad1@gmail.com>
2025-12-28 20:32:07 +0100

    Initial Commit

---
+++ SwiftTemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTemplate
+//
+//  Created by Adrian Neshad on 2025-12-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTemplate/SwiftTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTemplateApp.swift
+//  SwiftTemplate
+//
+//  Created by Adrian Neshad on 2025-12-28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
